bca913f8aa67aa84dcd8fe4f72d2c52fa70d112e
edayot <pro.e.dayot@gmail.com>
2024-08-06 10:16:24 +0200

    feat: tests load_vanilla

---
+++ tests/test_example.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+from pytest_insta import SnapshotFixture
+
+from beet import run_beet
+
+EXAMPLES = [f for f in os.listdir("examples") if not f.startswith("nosnap_")]
+
+
+@pytest.mark.parametrize("directory", EXAMPLES)
+def test_build(snapshot: SnapshotFixture, directory: str):
+    with run_beet(directory=f"examples/{directory}") as ctx:
+        assert snapshot("data_pack") == ctx.data
+        assert snapshot("resource_pack") == ctx.assets

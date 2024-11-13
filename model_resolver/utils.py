ae616f673bdae6ea70d9c6699b61ef107dcbbb90
edayot <pro.e.dayot@gmail.com>
2024-11-13 21:57:17 +0100

    fix: delete useless things

--- model_resolver/utils.py
+++ model_resolver/utils.py
@@ -37,18 +37,7 @@ class LightOptions(BaseModel):
 class ModelResolverOptions(BaseModel):
     """Model resolver options."""
 
-    load_vanilla: bool = False
-    resolve_vanilla_atlas: bool = False
     use_cache: bool = False
-    render_size: int = 256
     minecraft_version: str = "latest"
-    filter: Optional[list[str]] = []
-    special_filter: dict[str, str] = Field(default_factory=dict)
-    light: LightOptions = LightOptions()
-    save_namespace: Optional[str] = None
-    extra_block_entity_models: bool = False
-    colorize_blocks: bool = False
-    disable_missing_texture_error: bool = False
-    experimental_render_structure: bool = False
 
 

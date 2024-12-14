7e8b8c8c57baa7c31fb930e92bb026ac44a5da84
edayot <pro.e.dayot@gmail.com>
2024-12-14 20:13:41 +0100

    chore: test build

--- tests/test_example.py
+++ tests/test_example.py
@@ -11,6 +11,7 @@ EXAMPLES = [f for f in os.listdir("examples") if not f.startswith("nosnap_")]
 @pytest.mark.parametrize("directory", EXAMPLES)
 def test_build(snapshot: SnapshotFixture, directory: str):
     with run_beet(directory=f"examples/{directory}") as ctx:
-        pass
+        if os.environ.get("MODEL_RESOLVER_TEST_BUILD"):
+            return
         assert snapshot("data_pack") == ctx.data
         assert snapshot("resource_pack") == ctx.assets
